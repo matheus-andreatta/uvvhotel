43435f157fbcfa18cd3201cb878442ab7559771c
andreattamatheus <andreattamatheus25@gmail.com>
2020-05-27 21:56:20 -0300

    Arquivos index,hotel_form,regras e validacoes

---
+++ validacoes.js
@@ -0,0 +1,14 @@
+function validaDataSaida(){
+  var data = cadastro_usuario.data_saida;
+  console.log(data);
+  return
+  // window.alert('tá errado ai');
+}
+
+
+
+function computaValorDaEstadia(){
+
+}
+
+
