505db6b360495caaee227745017f308e14ec7e4d
andreattamatheus <andreattamatheus25@gmail.com>
2020-05-30 00:50:06 -0300

    Calculo da diaria, imagem adicionada e corrigido campos do hospede e hotel

--- validacoes.js
+++ validacoes.js
@@ -15,7 +15,33 @@ function validaDataSaida(){
 
 
 function computaValorDaEstadia(){
+  let dataEntrada = cadastro_usuario.data_entrada.value;
+  let dataSaida = cadastro_usuario.data_saida.value;
+  let valorDaDiáriaEmReais = 200;
 
+  //CONVERSÃO DA DATA
+  let dataEntradaConvert = new Date(dataEntrada);
+  let dataSaidaConvert = new Date(dataSaida);
+
+  //DURACAO EM MILISEGUNDOS
+  let duracaoDaEstadia = (dataSaidaConvert.getTime() - dataEntradaConvert.getTime());
+
+  //DURAÇÃO EM DIAS
+  let diasDeEstadia = (duracaoDaEstadia / (1000*60*60*24));
+
+  let totalDoValor = 'Calculando...';
+
+  if (diasDeEstadia > 0) {
+    totalDoValor = diasDeEstadia*valorDaDiáriaEmReais;
+  }
+
+  inserirNoHtml(totalDoValor);
+
+}
+
+
+function inserirNoHtml(valor){
+  document.getElementById('inputValor').innerHTML = valor;
 }
 
 
